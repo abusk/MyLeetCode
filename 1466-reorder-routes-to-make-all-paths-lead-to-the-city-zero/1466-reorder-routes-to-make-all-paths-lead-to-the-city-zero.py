b33407f60ba3abf2617b723aaee6b50ce4b021e7
abusk <abu.amupg24@gmail.com>
2024-03-17 10:15:05 +0530

    Time: 1043 ms (10.55%), Space: 61.2 MB (5.03%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,37 +1,37 @@
-# class Solution:
-#     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-#         adj = collections.defaultdict(set)
-#         for u, v in connections:
-#             adj[u].add((v, 1))
-#             adj[v].add((u, 0))
-        
-#         vi = []
-#         def dfs(node, c):
-#             if node in vi:
-#                 return c
-#             vi.append(node)
-#             for nei, is_connect in adj[node]:
-#                 if nei not in vi:
-#                     c += is_connect
-#                     c = dfs(nei, c)
-#             return c
-        
-#         return dfs(0, 0)
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        dt = defaultdict(list)
-        for a,b in connections:
-            dt[a].append((b,True))
-            dt[b].append((a,False))
-        ans = 0
-        q = [0]
-        vis = set()
-        while(q):
-            node = q.pop()
-            vis.add(node)
-            for nd,st in dt[node]:
-                if nd not in vis:
-                    q.append(nd)
-                    if st: ans+=1
-        return ans
+        adj = collections.defaultdict(set)
+        for u, v in connections:
+            adj[u].add((v, 1))
+            adj[v].add((u, 0))
+        
+        vi = set()
+        def dfs(node, c):
+            if node in vi:
+                return c
+            vi.add(node)
+            for nei, is_connect in adj[node]:
+                if nei not in vi:
+                    c += is_connect
+                    c = dfs(nei, c)
+            return c
+        
+        return dfs(0, 0)
+# class Solution:
+#     def minReorder(self, n: int, connections: List[List[int]]) -> int:
+#         dt = defaultdict(list)
+#         for a,b in connections:
+#             dt[a].append((b,True))
+#             dt[b].append((a,False))
+#         ans = 0
+#         q = [0]
+#         vis = set()
+#         while(q):
+#             node = q.pop()
+#             vis.add(node)
+#             for nd,st in dt[node]:
+#                 if nd not in vis:
+#                     q.append(nd)
+#                     if st: ans+=1
+#         return ans
         
