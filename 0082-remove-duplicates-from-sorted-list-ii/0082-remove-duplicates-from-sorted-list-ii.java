c88f9e205cdd14a7c7ed9eec4db21f34d4a683e0
abusk <abu.amupg24@gmail.com>
2024-03-22 05:38:14 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (34.92%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -8,25 +8,58 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         ListNode start = head;
+//         Map<Integer, Integer> map = new HashMap<>();
+//         while(start != null){
+//             map.compute(start.val, (k, v) -> (v == null) ? 1 : v + 1);
+//             start = start.next;
+//         }
+//         ListNode dummy = new ListNode(Integer.MIN_VALUE);
+//         ListNode nxt = dummy;
+//         ListNode tr = head;
+//         while(tr != null){
+//             if(map.get(tr.val) == 1){
+//                 ListNode node = new ListNode(tr.val);
+//                 nxt.next = node;
+//                 nxt = nxt.next;
+//             }
+//             tr = tr.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode start = head;
-        Map<Integer, Integer> map = new HashMap<>();
-        while(start != null){
-            map.compute(start.val, (k, v) -> (v == null) ? 1 : v + 1);
-            start = start.next;
-        }
-        ListNode dummy = new ListNode(Integer.MIN_VALUE);
-        ListNode nxt = dummy;
-        ListNode tr = head;
-        while(tr != null){
-            if(map.get(tr.val) == 1){
-                ListNode node = new ListNode(tr.val);
-                nxt.next = node;
-                nxt = nxt.next;
+        // Sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node 
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+        
+        while (head != null) {
+            // If it's a beginning of the duplicates sublist 
+            // skip all duplicates
+            if (head.next != null && head.val == head.next.val) {
+                // Move till the end of the duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+
+                // Skip all duplicates
+                pred.next = head.next;
+    
+            // otherwise, move predecessor
+            } else {
+                pred = pred.next;    
             }
-            tr = tr.next;
-        }
-        return dummy.next;
+                
+            // move forward
+            head = head.next;    
+        }  
+        return sentinel.next;
     }
 }
