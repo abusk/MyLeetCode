0821f19a2b4c0fda78706af62282f7868a4cd456
abusk <abu.amupg24@gmail.com>
2024-04-19 08:49:11 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (8.06%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int trailingZeroes(int n) {
+public int trailingZeroes(int n) {
     int zeroCount = 0;
-    for (int i = 5; i <= n; i += 5) {
-        int currentFactor = i;
-        while (currentFactor % 5 == 0) {
-            zeroCount++;
-            currentFactor /= 5;
-        }
+    long currentMultiple = 5;
+    while (n > 0) {
+        n /= 5;
+        zeroCount += n;
     }
     return zeroCount;
 }
