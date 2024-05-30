7ce8e068cf178141023839a95c1d0a5e1d1fa741
Abu Taleb Sekh <abu.amupg24@gmail.com>
2024-05-30 09:48:00 +0530

    Time: 2 ms (77.44%), Space: 41.4 MB (19.49%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,20 +1,50 @@
+// class Solution {
+//     public int countTriplets(int[] arr) {
+//         int c = 0;
+//         int l = arr.length;
+//         for(int k = 0; k<l; k++) {    
+//             int a = 0;
+//             for(int i = k; i<l; i++) {
+//                 a ^= arr[i];
+//                 int b = 0;
+//                 for(int j = i+1; j<l; j++) {
+//                     b^=arr[j];
+//                     if(a == b) {
+//                         c++;
+//                     }
+//                 }
+//             }
+//         }
+//         return c;
+//     }
+// }
 class Solution {
+
     public int countTriplets(int[] arr) {
-        int c = 0;
-        int l = arr.length;
-        for(int k = 0; k<l; k++) {    
-            int a = 0;
-            for(int i = k; i<l; i++) {
-                a ^= arr[i];
-                int b = 0;
-                for(int j = i+1; j<l; j++) {
-                    b^=arr[j];
-                    if(a == b) {
-                        c++;
-                    }
-                }
-            }
+        int size = arr.length;
+        int count = 0;
+        int prefix = 0;
+
+        // Maps to store counts and totals of XOR values encountered
+        Map<Integer, Integer> countMap = new HashMap<>();
+        countMap.put(0, 1);
+        Map<Integer, Integer> totalMap = new HashMap<>();
+
+        // Iterating through the array
+        for (int i = 0; i < size; ++i) {
+            // Calculating XOR prefix
+            prefix ^= arr[i];
+
+            // Calculating contribution of current element to the result
+            count +=
+            countMap.getOrDefault(prefix, 0) * i -
+            totalMap.getOrDefault(prefix, 0);
+
+            // Updating total count of current XOR value
+            totalMap.put(prefix, totalMap.getOrDefault(prefix, 0) + i + 1);
+            countMap.put(prefix, countMap.getOrDefault(prefix, 0) + 1);
         }
-        return c;
+
+        return count;
     }
 }
