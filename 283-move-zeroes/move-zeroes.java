cb97358d03a4987f8ea33a5c635715d05775221d
Abu Taleb Sekh <abu.amupg24@gmail.com>
2024-07-25 10:36:20 +0530

    Time: 2 ms (83.04%) | Memory: 46.2 MB (28.96%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zc = 0;
+        for(int i= 0; i<nums.length; i++) {
+            if(nums[i] == 0) {
+                zc++;
+            }
+        }
+    
+        int j = 0;
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
