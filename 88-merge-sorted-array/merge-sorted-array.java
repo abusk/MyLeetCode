f0e22a00d071838e770832a9b2e0fb2d234105ce
Abu Taleb Sekh <abu.amupg24@gmail.com>
2024-10-30 12:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (61.55%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,28 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int lastIndex = m + n - 1;
+//         int nums2Index = n -1;
+//         int nums1Index = m -1;
+//         while (nums2Index >= 0) {
+//             if (nums1Index >= 0 && nums2[nums2Index] < nums1[nums1Index]) {
+//               nums1[lastIndex--] = nums1[nums1Index--];
+//             } else {
+//                 nums1[lastIndex--] = nums2[nums2Index--];
+//             }
+//         }
+//     }
+// }
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int lastIndex = m + n - 1;
+        int nums2Index = n -1;
+        int nums1Index = m -1;
+        while (nums2Index >= 0) {
+            if (nums1Index >= 0 && nums2[nums2Index] < nums1[nums1Index]) {
+              nums1[lastIndex--] = nums1[nums1Index--];
+            } else {
+                nums1[lastIndex--] = nums2[nums2Index--];
+            }
+        }
+    }
+}
