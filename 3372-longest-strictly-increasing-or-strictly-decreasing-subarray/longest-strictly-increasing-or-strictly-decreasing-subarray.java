2638782f91f2de86707a855b1e283c111ae9d4b8
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-02-03 09:33:18 +0530

    Time: 1 ms (93.94%) | Memory: 42.5 MB (91.92%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,33 +1,28 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        int[] state = new int[nums.length];
-        int[] sLen = new int[nums.length];
+        int state = 0;
         int max = 1;
-        for(int i = 0; i< nums.length; i++) {
-            sLen[i] = 1;
-        }
+        int lmax = 1;
         for(int i = 1; i<nums.length; i++) {
-            if(nums[i] > nums[i-1]) {
-                state[i] = 1;
-            } else if(nums[i] < nums[i-1]) {
-                state[i] = -1;
+            if(nums[i] > nums[i-1] && state == 0) {
+                state = 1;
+                lmax = 2;  
+            } else if(nums[i] < nums[i-1] && state == 0) {
+                state = -1;
+                lmax = 2; 
+            } else if((nums[i] > nums[i-1] && state == 1) || (nums[i] < nums[i-1] && state == -1)){
+                lmax += 1; 
+            } else if(nums[i] > nums[i-1] && state == -1){
+                state = 1;
+                lmax = 2; 
+            } else if(nums[i] < nums[i-1] && state == 1){
+                state = -1;
+                lmax = 2; 
             } else {
-                state[i] = 0;
+                state = 0;
+                lmax = 1;
             }
-        }
-        for(int i = 1; i<nums.length; i++) {
-            if(state[i] != 0 && state[i] == state[i-1]) {
-                sLen[i] = sLen[i-1] + 1;
-            } 
-        }
-        for(int i = 0; i<nums.length; i++) {
-            max= Math.max(max, sLen[i]);
-        }
-        for(int i= 0; i<nums.length; i++) {
-            if(state[i] != 0) {
-                max = max+1;
-                break;
-            }
+            max = Math.max(max, lmax);
         }
         return max;
     }
