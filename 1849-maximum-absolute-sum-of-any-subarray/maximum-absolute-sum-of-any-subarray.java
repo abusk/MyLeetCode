a677a3ad7f6bf8a35622905c6cedfa2eeac3510b
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-02-26 17:42:45 +0530

    Time: 8 ms (12.96%) | Memory: 56.2 MB (85.98%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -1,37 +1,52 @@
+// class Solution {
+
+//     public int maxAbsoluteSum(int[] nums) {
+//         int minPrefixSum = Integer.MAX_VALUE, maxPrefixSum = Integer.MIN_VALUE;
+//         int prefixSum = 0, maxAbsSum = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             // Prefix sum from index 0 to i
+//             prefixSum += nums[i];
+
+//             // Minimum & Maximum prefix sum we have seen so far
+//             minPrefixSum = Math.min(minPrefixSum, prefixSum);
+//             maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+
+//             if (prefixSum >= 0) {
+//                 // If the prefixSum is positive, we will get the difference between prefixSum &
+//                 // minPrefixSum
+//                 maxAbsSum = Math.max(
+//                     maxAbsSum,
+//                     Math.max(prefixSum, prefixSum - minPrefixSum)
+//                 );
+//             } else if (prefixSum <= 0) {
+//                 // If the prefixSum is negative, we will get the absolute difference between
+//                 // prefixSum & maxPrefixSum
+//                 maxAbsSum = Math.max(
+//                     maxAbsSum,
+//                     Math.max(
+//                         Math.abs(prefixSum),
+//                         Math.abs(prefixSum - maxPrefixSum)
+//                     )
+//                 );
+//             }
+//         }
+
+//         return maxAbsSum;
+//     }
+// }
+
 class Solution {
-
     public int maxAbsoluteSum(int[] nums) {
-        int minPrefixSum = Integer.MAX_VALUE, maxPrefixSum = Integer.MIN_VALUE;
-        int prefixSum = 0, maxAbsSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            // Prefix sum from index 0 to i
-            prefixSum += nums[i];
-
-            // Minimum & Maximum prefix sum we have seen so far
-            minPrefixSum = Math.min(minPrefixSum, prefixSum);
-            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
-
-            if (prefixSum >= 0) {
-                // If the prefixSum is positive, we will get the difference between prefixSum &
-                // minPrefixSum
-                maxAbsSum = Math.max(
-                    maxAbsSum,
-                    Math.max(prefixSum, prefixSum - minPrefixSum)
-                );
-            } else if (prefixSum <= 0) {
-                // If the prefixSum is negative, we will get the absolute difference between
-                // prefixSum & maxPrefixSum
-                maxAbsSum = Math.max(
-                    maxAbsSum,
-                    Math.max(
-                        Math.abs(prefixSum),
-                        Math.abs(prefixSum - maxPrefixSum)
-                    )
-                );
-            }
+        int max_sum = 0, min_sum = 0, max_absolute_sum = 0;
+        
+        for (int num : nums) {
+            max_sum = Math.max(num, max_sum + num);  // Maximum sum subarray
+            min_sum = Math.min(num, min_sum + num);  // Minimum sum subarray
+            max_absolute_sum = Math.max(max_absolute_sum, Math.abs(max_sum));
+            max_absolute_sum = Math.max(max_absolute_sum, Math.abs(min_sum));
         }
-
-        return maxAbsSum;
+        
+        return max_absolute_sum;
     }
 }
