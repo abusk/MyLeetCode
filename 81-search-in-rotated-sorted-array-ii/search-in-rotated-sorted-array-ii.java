a5605fad4f1f55429fe442608ef3d4aafcd76ef1
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-03-15 18:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (50.33%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,40 +1,55 @@
 class Solution {
-  public static boolean search(int[] nums, int target) {
-    if (null == nums || 0 == nums.length)
-      return false;
-    
-    int lo = 0;
-    int hi = nums.length - 1;
-    while (lo <= hi) {
-      // to avoid duplicates
-      while (lo < hi && nums[lo] == nums[lo + 1])
-        ++lo;
-      while (lo < hi && nums[hi] == nums[hi - 1])
-        --hi;
-      
-      // the code below is exactly the same with Problem 33.
-      int mid = lo + (hi - lo) / 2;
-      if (nums[mid] == target)
-        return true;
-      
-      if (nums[mid] >= nums[lo]) {
-        if (target >= nums[lo] && target < nums[mid])
-          hi = mid - 1;
-        else
-          lo = mid + 1;
-      } else {
-        if (target <= nums[hi] && target > nums[mid])
-          lo = mid + 1;
-        else
-          hi = mid - 1;
-      }
+    public static boolean search(int[] nums, int target) {
+        int p = findPivot(nums);
+
+        if(p != -1) {
+            return findBS(nums, 0, p, target) || findBS(nums, p, nums.length-1, target);
+        }
+        return findBS(nums,0, nums.length-1, target);
     }
-    
-    return false;
-  }
+
+    public static int findPivot(int[] nums) {
+        int s = 0;
+        int l = nums.length-1;
+        while (s <= l) {
+            while (s < l && nums[s] == nums[s + 1])
+                ++s;
+            while (s < l && nums[l] == nums[l - 1])
+                --l;
+
+            int mid = (s + l) /2;
+            if(mid > 0 && (nums[mid] < nums[mid-1])) {
+                return mid;
+            }
+            if(nums[mid] == nums[s]){
+                s++;
+                continue;
+            }
+            if(nums[mid] > nums[0]) {
+                s = mid +1;
+            } else {
+                l = mid -1;
+            }
+        }
+        return -1;
+    }
+    public static boolean findBS(int[] nums, int s, int l, int target) {
+        while (s <= l) {
+            int mid = (s+l)/2;
+            if(nums[mid] == target) {
+                return true;
+            }
+            if(nums[mid] < target) {
+                s = mid+1;
+            } else {
+                l = mid -1;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        int [] nums = {1,0,1,1,1};
+        int [] nums = {1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1};
         System.out.println(search(nums, 0));
     }
 }
