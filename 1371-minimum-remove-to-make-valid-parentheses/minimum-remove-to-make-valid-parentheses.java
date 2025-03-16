659253d6857297ba30a4bdeb50e04ebc575b9f0e
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-03-16 21:01:31 +0530

    Time: 24 ms (39.93%) | Memory: 45.3 MB (91.81%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,27 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
-        Stack<Pair<Character, Integer>> st = new Stack<>();
-        StringBuilder sb = new StringBuilder(s);
+    record Pair(Character key, Integer val) {}
+    public static String minRemoveToMakeValid(String s) {
+        Stack<Pair> st = new Stack<>();
         for(int i = 0; i<s.length(); i++) {
             char ch = s.charAt(i);
             if(ch == '(') {
-                st.push(new Pair<>(ch, i));
+                st.push(new Pair(ch, i));
             } else if(ch == ')') {
-                if(!st.isEmpty() && st.peek().getKey() == '('){
+                if (!st.isEmpty() && st.peek().key == '(') {
                     st.pop();
                 } else {
-                    st.push(new Pair<>(ch, i));
+                    st.push(new Pair(ch, i));
                 }
-                
             }
         }
-        for(Pair<Character, Integer> p : st) {
-            sb.setCharAt(p.getValue(), '#');
+        StringBuilder sb = new StringBuilder(s);
+        for (Pair p : st) {
+            sb.setCharAt(p.val, '#');
         }
-
         return sb.toString().replace("#", "");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minRemoveToMakeValid("lee(t(c)o)de)"));
     }
 }
