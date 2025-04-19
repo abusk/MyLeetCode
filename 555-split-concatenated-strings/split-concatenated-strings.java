3152a898499e813460e7c02ce8dbe53903589b84
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-04-19 11:52:59 +0530

    Time: 19 ms (100.00%) | Memory: 45.2 MB (53.85%) - LeetSync

--- 555-split-concatenated-strings/split-concatenated-strings.java
+++ 555-split-concatenated-strings/split-concatenated-strings.java
@@ -1,64 +1,30 @@
 class Solution {
     public String splitLoopedString(String[] strs) {
-        char max = 'a';
-        for(int i = 0; i < strs.length; i++){
-            String curr = strs[i];
-            for(char chr : curr.toCharArray()){
-                if(chr > max){
-                    max = chr;
-                }
-            }
-            String reversestring = reverse(curr);
-            if(reversestring.compareTo(curr) > 0){
-                strs[i] = reversestring;
-            }
-        }
-
-        String result = new String();
-        String maxstring = String.valueOf(max);
-
-        for(int i = 0; i < strs.length; i++){
-            String cur = strs[i];
-            String reversecur = reverse(cur);
-            if(cur.indexOf(maxstring) == -1 && reversecur.indexOf(maxstring) == -1){
-                continue;
-            }
-            for(String string : new String[]{cur, reversecur}){
-                for(int k = 0; k < string.length(); k++){
-                    if(string.charAt(k) != max){
-                        continue;
-                    }
-                    StringBuilder sb = new StringBuilder(string.substring(k));
-
-                    for(int j = i + 1; j < strs.length; j++){
-                        sb.append(strs[j]);
-                    }
-
-                    for(int j = 0; j < i; j++){
-                        sb.append(strs[j]);
-                    }
-                    
-                    sb.append(string.substring(0, k));
-                    String newstring = sb.toString();
-                    if(newstring.compareTo(result) > 0){
-                        result = newstring;
-                    }
-                }
-            }
-        }
-        return result;
+    String stot = "";
+    for (int i = 0; i < strs.length; i++) {
+        String rever = new StringBuffer(strs[i]).reverse().toString();
+        if (rever.compareTo(strs[i]) > 0) strs[i] = rever;
+        stot = stot + strs[i];
     }
-    private String reverse(String string){
-        char[] array = string.toCharArray();
-        int left = 0;
-        int right = array.length - 1;
-        while(left < right){
-            char temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-            left++;
-            right--;
+    int start = 0;
+    String sol = stot;
+    char maxch = 'a';
+    for (int i = 0; i < strs.length; i++) {
+        int n = strs[i].length();
+        start += n;
+        String rever = new StringBuffer(strs[i]).reverse().toString();
+        String other_strs = stot.substring(start) + stot.substring(0, start - n);
+        for (int j = 0; j < n; j++) {
+            if (strs[i].charAt(j) - maxch >= 0) {
+                maxch = strs[i].charAt(j);
+                String s1 = strs[i].substring(j) + other_strs + strs[i].substring(0, j);
+                String s2 = rever.substring(n-1-j) + other_strs + rever.substring(0, n-1-j);
+                if (s1.compareTo(sol) > 0) sol = s1;
+                if (s2.compareTo(sol) > 0) sol = s2;
+            }
         }
-        return new String(array);
-    }
+    }    
+    
+    return sol;
+}
 }
