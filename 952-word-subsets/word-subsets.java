415093c7b668c17a61abfd63642e43d374568808
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-04-21 21:04:00 +0530

    Time: 13 ms (80.97%) | Memory: 54.6 MB (65.30%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,27 +1,32 @@
 class Solution {
-
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] bmax = count("");
-        for (String b : words2) {
-            int[] bCount = count(b);
-            for (int i = 0; i < 26; ++i) bmax[i] = Math.max(bmax[i], bCount[i]);
+        int[] w2c = new int[26];
+        for(String w2 : words2) {
+            int[] tmp = new int[26];
+            for(char ch : w2.toCharArray()) {
+                tmp[ch -'a']++;
+            }
+            for(int i = 0; i<26; i++) {
+                w2c[i] = Math.max(w2c[i], tmp[i]);
+            }
         }
-
-        List<String> ans = new ArrayList();
-        search: for (String a : words1) {
-            int[] aCount = count(a);
-            for (int i = 0; i < 26; ++i) if (
-                aCount[i] < bmax[i]
-            ) continue search;
-            ans.add(a);
+        List<String> ans = new ArrayList<>();
+        for(String w1 : words1) {
+            int[] tmp = new int[26];
+            for(char ch : w1.toCharArray()) {
+                tmp[ch -'a']++;
+            }
+            boolean flag = true;
+            for(int i = 0; i<26; i++) {
+                if(tmp[i] < w2c[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                ans.add(w1);
+            }
         }
-
-        return ans;
-    }
-
-    public int[] count(String S) {
-        int[] ans = new int[26];
-        for (char c : S.toCharArray()) ans[c - 'a']++;
         return ans;
     }
 }
