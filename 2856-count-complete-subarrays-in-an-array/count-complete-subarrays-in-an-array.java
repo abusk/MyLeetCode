aafde470cb2e3d86f3102bfd4fe63a8f81b732d0
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-04-24 09:31:05 +0530

    Time: 7 ms (89.07%) | Memory: 45.3 MB (34.20%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,31 +1,39 @@
 class Solution {
-
     public int countCompleteSubarrays(int[] nums) {
-        int res = 0;
-        Map<Integer, Integer> cnt = new HashMap<>();
-        int n = nums.length;
-        int right = 0;
-        int distinct = new HashSet<>(
-            Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new))
-        ).size();
+        Set<Integer> set = new HashSet<>();
 
-        for (int left = 0; left < n; left++) {
-            if (left > 0) {
-                int remove = nums[left - 1];
-                cnt.put(remove, cnt.get(remove) - 1);
-                if (cnt.get(remove) == 0) {
-                    cnt.remove(remove);
-                }
-            }
-            while (right < n && cnt.size() < distinct) {
-                int add = nums[right];
-                cnt.put(add, cnt.getOrDefault(add, 0) + 1);
-                right++;
-            }
-            if (cnt.size() == distinct) {
-                res += (n - right + 1);
-            }
+        for(int val : nums){
+            set.add(val);
         }
-        return res;
+
+        int k = set.size();
+
+        int i = 0;
+        
+        int j = 0;
+
+        int ans = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        while(j < nums.length){
+
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+
+            while(map.size() == k){
+
+                ans += (nums.length - j);
+
+                map.put(nums[i],map.get(nums[i])-1);
+
+                if(map.get(nums[i]) == 0){
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return ans;
     }
 }
