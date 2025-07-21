96d30d398b0cf482cbe56a5e4aaafb6eeba0ef7b
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-07-21 09:02:13 +0530

    Time: 35 ms (57.83%) | Memory: 45.7 MB (78.54%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,26 +1,42 @@
 class Solution {
     public record Pair(char ch, int count){}
     public String makeFancyString(String s) {
-        Stack<Pair> st = new Stack<>();
-        for(char ch : s.toCharArray()) {
-            if(st.isEmpty()) {
-                Pair p = new Pair(ch, 1);
-                st.push(p);
-            } else {
-                Pair peek = st.peek();
-                if(ch != peek.ch) {
-                    Pair p = new Pair(ch, 1);
-                    st.push(p); 
-                } else if(ch == peek.ch && peek.count < 2){
-                    Pair p = new Pair(ch, peek.count+1);
-                    st.push(p); 
-                }
+        // Stack<Pair> st = new Stack<>();
+        // for(char ch : s.toCharArray()) {
+        //     if(st.isEmpty()) {
+        //         Pair p = new Pair(ch, 1);
+        //         st.push(p);
+        //     } else {
+        //         Pair peek = st.peek();
+        //         if(ch != peek.ch) {
+        //             Pair p = new Pair(ch, 1);
+        //             st.push(p); 
+        //         } else if(ch == peek.ch && peek.count < 2){
+        //             Pair p = new Pair(ch, peek.count+1);
+        //             st.push(p); 
+        //         }
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(Pair p : st) {
+        //     sb.append(p.ch);
+        // }
+        // return sb.toString();
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+        int count = 1;
+        for(int i=0;i<n;i++){
+            if(i>0 && s.charAt(i) == s.charAt(i-1)){
+                count ++;
+            }
+            else{
+                count = 1;
+            }
+
+            if(count<=2){
+                ans.append(s.charAt(i));
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(Pair p : st) {
-            sb.append(p.ch);
-        }
-        return sb.toString();
+        return ans.toString();
     }
 }
