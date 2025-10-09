aa1835916f37bb2ffa012cef13be09680cd48b61
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-10-09 08:15:36 +0530

    Time: 1 ms (94.73%) | Memory: 41.9 MB (46.08%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -1,63 +1,32 @@
-// class Solution {
-//     public boolean validWordAbbreviation(String word, String abbr) {
-//         int wi = 0;
-//         for(int i = 0; i<abbr.length(); i++) {
-//             Character ch = abbr.charAt(i);
-//             if(ch == '0') {
-//                 return false;
-//             }
-//             if(Character.isDigit(ch)) {
-//                 int k = i;
-//                 while (Character.isDigit(ch)) {
-//                     i++;
-//                     ch = abbr.charAt(i);
-//                 }
-//                 int num = getNumber(abbr, k, i);
-//                 wi = Math.min(wi+num, word.length()-1);
-//                 i--;
-//             } else {
-//                 if(abbr.charAt(i) != word.charAt(wi)) {
-//                     return false;
-//                 }
-//                 wi++;
-//             }
-//         }
-//         if(wi != word.length()) {
-//             return false;
-//         }
-//         return true;
-//     }
-
-//     public int getNumber(String abbr, int i, int j) {
-//         return Integer.parseInt(abbr.substring(i, j));
-//     }
-// }
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if(abbr.length() > word.length()) return false;
-        int i = 0, j = 0;
-        while(i < word.length() && j < abbr.length()){
-            Character wc = word.charAt(i);
-            Character ac = abbr.charAt(j);
-            // if both characters are different, return false
-            if(Character.isLetter(ac) && wc != ac) return false;
-            else{
-                // encountering a digit
-                if(Character.isDigit(ac)){
-                    int a = ac - '0';
-                    if(a == 0) return false;
-                    // while we get digits, we create bigger number
-                    while(j + 1 < abbr.length() && Character.isDigit(abbr.charAt(j + 1))){
-                        a = a * 10 + abbr.charAt(j + 1) - '0';
-                        j++;
-                    }
-                    // jump i end of digits position
-                    i += a - 1;
+        int wl = word.length();
+        int al = abbr.length();
+        int ai = 0;
+        int wi = 0;
+        while(ai < al && wi < wl) {
+            char wch = word.charAt(wi);
+            char ach = abbr.charAt(ai);
+            if(!Character.isDigit(ach)) {
+                if(wch != ach) {
+                    return false;
+                } else {
+                    ai++;
+                    wi++;
+                    continue;
                 }
+            } else {
+                if(ach == '0') {
+                    return false;
+                }
+                int num = 0;
+                while(ai < al && Character.isDigit(abbr.charAt(ai))) {
+                    num = num * 10 + (abbr.charAt(ai) - '0');
+                    ai++;
+                }
+                wi += num;
             }
-            j++;
-            i++;
         }
-        return i == word.length() && j == abbr.length();
+        return wi == wl && ai == al;
     }
 }
