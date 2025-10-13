79870ebe337eccafc601f73742548afb74505757
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-10-13 10:29:13 +0530

    Time: 149 ms (26.53%) | Memory: 46.8 MB (7.67%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,55 +1,75 @@
 class Solution {
-    public static String longestPalindrome(String s) {
-        int len = s.length();
-        boolean [][] dp = new boolean[len][len];
-        int[] ans = new int[]{0, 0};
-        for(int i = 0; i<len; i++) {
+    public String longestPalindrome(String s) {
+        // Map<String, Boolean> memo = new HashMap<>();
+        // int maxL = -1;
+        // String ans = "";
+        // for(int j = 0; j <s.length(); j++) {
+        //     for(int i = 0; i <= j; i++) {
+        //         if(dp(s, i, j, memo)) {
+        //             if(maxL < j - i) {
+        //                 maxL = j-i;
+        //                 ans = s.substring(i, j+1);
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+        int maxL = 1;
+        int start = 0;
+        int end = 0;
+        boolean [][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i<s.length(); i++) {
             dp[i][i] = true;
         }
-        for(int i = 0; i<len-1; i++) {
+        for(int i = 0; i<s.length()-1; i++) {
             if(s.charAt(i) == s.charAt(i+1)) {
+                maxL = 2;
+                start = i;
+                end = i+1;
                 dp[i][i+1] = true;
-                ans[0] = i;
-                ans[1] = i+1;
+            } else {
+                dp[i][i+1] =false;
             }
         }
-        for(int i = 2; i<len; i++) {
-            for(int start = 0; start < len - i; start++) {
-                int end = start + i;
-                if(s.charAt(start) == s.charAt(end) && dp[start+1][end-1]) {
-                    dp[start][end] = true;
-                    if(ans[1] - ans[0] < end - start) {
-                        ans[0] = start;
-                        ans[1] = end;
+        for(int w = 2; w<s.length(); w++) {
+            for(int i = 0; i<s.length()-w; i++) {
+                int j = i + w;
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]) {
+                    if(maxL < w+1) {
+                        start = i;
+                        end = j;
+                        maxL = w+1;
                     }
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = false;
                 }
             }
         }
-        return s.substring(ans[0], ans[1]+1);
-    }
-    public static String dp(String s, int i, int j) {
-        if(i > j) {
-            return "";
-        }
-        if(isPal(s, i, j)) {
-            return s.substring(i, j+1);
-        }
-        String p1 = dp(s, i+1, j);
-        String p2 = dp(s, i, j-1);
-        return p1.length() >= p2.length() ? p1 : p2;
-    }
-    public static boolean isPal(String s, int i, int j) {
-        while(i < j) {
-            if(s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        return s.substring(start, end+1);
     }
 
-    public static void main(String[] args) {
-        System.out.println(longestPalindrome("cbbd"));
-    }
+    // public boolean dp(String s, int i, int j, Map<String, Boolean> memo) {
+    //     String key = i + ":" + j;
+    //     if(memo.containsKey(key)) {
+    //         return memo.get(key);
+    //     }
+    //     if(i < 0 || j >= s.length()) {
+    //         return false;
+    //     }
+    //     if(i == j) {
+    //         return true;
+    //     }
+    //     if(i+1 == j && s.charAt(i) == s.charAt(j)) {
+    //         return true;
+    //     }
+    //     boolean result = false;
+    //     if(s.charAt(i) != s.charAt(j)) {
+    //         result = false;
+    //     } else {
+    //         result =  dp(s, i+1, j-1, memo);
+    //     }
+    //     memo.put(key, result);
+    //     return result;
+    // }
 }
