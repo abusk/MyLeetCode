e7909fccff440bfb49fc39b1fb7ffb60eaff85ad
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-10-14 10:24:59 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (54.38%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,36 +10,31 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(-1);
+        ListNode next = ans;
         int carry = 0;
-        ListNode result = null;
-        ListNode last = null;
-        while (l1!= null || l2 != null) {
-            int sum = 0;
-            if(l1 == null) {
-                sum = l2.val + carry;
-                l2 = l2.next;
-            } else if(l2 == null) {
-                sum = l1.val + carry;
-                l1 = l1.next;
-            } else {
-                sum = l1.val + l2.val + carry;
-                l1 = l1.next;
-                l2 = l2.next;
-            }
-            int v = sum % 10;
-            carry = sum / 10;
-            if(result == null) {
-                result = new ListNode(v);
-                last = result;
-            } else {
-                last.next = new ListNode(v);
-                last = last.next;
-            }
+        while(l1 != null && l2 != null) {
+            int total = (l1.val + l2.val + carry);
+            int val = total % 10;
+            carry = total /10;
+            next.next = new ListNode(val);
+            next = next.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        if(carry > 0) {
-            last.next = new ListNode(carry);
-            last = last.next;
+        ListNode rest = l1 != null ? l1 : l2;
+        while(rest != null) {
+            int total = (rest.val + carry);
+            int val = total % 10;
+            carry = total /10;
+            next.next = new ListNode(val);
+            next = next.next;
+            rest = rest.next;
         }
-        return result;
+        if(carry != 0) {
+            next.next = new ListNode(carry);
+        }
+
+        return ans.next;
     }
 }
