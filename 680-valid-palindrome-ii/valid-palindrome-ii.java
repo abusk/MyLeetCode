149abee287ef254f38621c2c23ba052b3dee0c34
Abu Taleb Sekh <abu.amupg24@gmail.com>
2025-10-14 09:57:52 +0530

    Time: 11 ms (5.46%) | Memory: 49.3 MB (5.11%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        return check(s, 0, s.length()-1, 1);
+        return dp(s, 0, s.length()-1, false);
     }
-    public boolean check(String s, int i, int j, int c) {
+    public boolean dp(String s, int i, int j, boolean deleted) {
         if(i >= j) {
             return true;
         }
-        if(s.charAt(i) == s.charAt(j)) {
-            return check(s, i+1, j-1, c);
-        }
-        if(s.charAt(i) != s.charAt(j) &&  c <= 0) {
+        if(deleted && s.charAt(i) != s.charAt(j)) {
             return false;
         }
-        return check(s, i+1, j, c-1) || check(s, i, j-1, c-1);
+        if(s.charAt(i) != s.charAt(j)) {
+            return dp(s, i+1, j, true) || dp(s, i, j-1, true);
+        }
+        return dp(s, i+1, j-1, deleted);
     }
 }
