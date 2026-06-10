897c9e1b0bb441ff8496b898ed19a6bf8bd17735
Abu Taleb Sekh <abu.amupg24@gmail.com>
2026-06-10 19:58:19 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (81.07%) - LeetSync

---
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfGoodIntegers(int n, int k) {
+        int start = Math.max(1, n-k);
+        int end = n + k;
+        int sum = 0;
+        for( int i = start; i<= end; i++) {
+            int and = n & i;
+            if(and == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
